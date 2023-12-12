5cd9f3d10358de37a2991c56e358c365fde8c321
RachelPoe <rapo3461@colorado.edu>
2023-12-12 14:29:51 -0700

    js midterm css update

--- js-midterm/js-midterm.js
+++ js-midterm/js-midterm.js
@@ -1,3 +1,5 @@
+//add submit and clear button using innerHTML
+
 const box1 = document.querySelector('.box1');
 const box2 = document.querySelector('.box2');
 const box3 = document.querySelector('.box3');
